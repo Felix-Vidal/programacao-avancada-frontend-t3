8e9207e2195d6de01577d649ef669f3d88d9cfdb
joao.vidal <112819239+Felix-Vidal@users.noreply.github.com>
2023-07-24 18:27:26 -0500

    melhoria na animação do circle

--- bootstrap-project-my/scripts.js
+++ bootstrap-project-my/scripts.js
@@ -1,132 +1,57 @@
+htmlIdArray = ["circleA", "circleB", "circleC", "circleD"];
+circleInternSizeNumber = [27, 66, 4, 222, 60];
 
-let containerA = new ProgressBar.Circle(circleA, {
-  color: '#aaa',
+createCirclesUsingArrays(htmlIdArray, circleInternSizeNumber);
 
-  strokeWidth: 4,
-  trailWidth: 1,
-  easing: 'easeInOut',
-  duration: 1400,
-  text: {
-    autoStyleContainer: true
-  },
-  from: { color: '#aaa', width: 1 },
-  to: { color: '#333', width: 4 },
-
-  step: function(state, circle) {
-    circle.path.setAttribute('stroke', state.color);
-    circle.path.setAttribute('stroke-width', state.width);
-
-    let value = Math.round(circle.value() * 564);
-    if (value === 0) {
-      circle.setText('');
-    } else {
-      circle.setText(value);
+function createCirclesUsingArrays(htmlIdArray, circleInternSizeNumber) {
+    for (let i = 0; i < htmlIdArray.length; i++) {
+      activeCircle(
+        returnNewProgressBarObj(returnHtmlElementId(htmlIdArray[i]), circleInternSizeNumber[i])
+      );
     }
+}
 
+function activeCircle(progressBar) {
+    progressBar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+    progressBar.text.style.fontSize = "2rem";
+    progressBar.animate(1.0); // Number from 0.0 to 1.0
   }
-});
+  
+function returnNewProgressBarObj(circleName, internSize) {
+  return new ProgressBar.Circle(circleName, returnCircleObj(internSize));
+}
 
-let containerB = new ProgressBar.Circle(circleB, {
-  color: '#aaa',
+function returnCircleObj(internSize){
+    return {
+        color: "#aaa",
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 4,
+        trailWidth: 1,
+        easing: "easeInOut",
+        duration: 1400,
+        text: {
+          autoStyleContainer: true,
+        },
+        from: { color: "#aaa", width: 1 },
+        to: { color: "#333", width: 4 },
+        // Set default step function for all animate calls
+        step: function (state, circle) {
+            circle.path.setAttribute("stroke", state.color);
+            circle.path.setAttribute("stroke-width", state.width);
+      
+            let value = Math.round(circle.value() * internSize);
+            if (value === 0) {
+              circle.setText("");
+            } else {
+              circle.setText(value);
+            }
+          }}   
+}    
 
-  strokeWidth: 4,
-  trailWidth: 1,
-  easing: 'easeInOut',
-  duration: 1400,
-  text: {
-    autoStyleContainer: true
-  },
-  from: { color: '#aaa', width: 1 },
-  to: { color: '#333', width: 4 },
-
-  step: function(state, circle) {
-    circle.path.setAttribute('stroke', state.color);
-    circle.path.setAttribute('stroke-width', state.width);
-
-    let value = Math.round(circle.value() * 75);
-    if (value === 0) {
-      circle.setText('');
-    } else {
-      circle.setText(value);
-    }
-
-  }
-});
-
-let containerC = new ProgressBar.Circle(circleC, {
-  color: '#aaa',
-
-  strokeWidth: 4,
-  trailWidth: 1,
-  easing: 'easeInOut',
-  duration: 1400,
-  text: {
-    autoStyleContainer: true
-  },
-  from: { color: '#aaa', width: 1 },
-  to: { color: '#333', width: 4 },
-
-  step: function(state, circle) {
-    circle.path.setAttribute('stroke', state.color);
-    circle.path.setAttribute('stroke-width', state.width);
-
-    let value = Math.round(circle.value() * 313);
-    if (value === 0) {
-      circle.setText('');
-    } else {
-      circle.setText(value);
-    }
-
-  }
-});
-
-let containerD = new ProgressBar.Circle(circleD, {
-  color: '#aaa',
-
-  strokeWidth: 4,
-  trailWidth: 1,
-  easing: 'easeInOut',
-  duration: 1400,
-  text: {
-    autoStyleContainer: true
-  },
-  from: { color: '#aaa', width: 1 },
-  to: { color: '#333', width: 4 },
-
-  step: function(state, circle) {
-    circle.path.setAttribute('stroke', state.color);
-    circle.path.setAttribute('stroke-width', state.width);
-
-    let value = Math.round(circle.value() * 768);
-    if (value === 0) {
-      circle.setText('');
-    } else {
-      circle.setText(value);
-    }
-
-  }
-});
-
-containerA.text.style.fontFamily = 'Helvetica, sans-serif';
-containerA.text.style.fontSize = '2rem';
-
-containerB.text.style.fontFamily = 'Helvetica, sans-serif';
-containerB.text.style.fontSize = '2rem';
-
-containerC.text.style.fontFamily = 'Helvetica, sans-serif';
-containerC.text.style.fontSize = '2rem';
-
-containerD.text.style.fontFamily = 'Helvetica, sans-serif';
-containerD.text.style.fontSize = '2rem';
-
-
-containerA.animate(1.0); 
-containerB.animate(1.0);
-containerC.animate(1.0); 
-containerD.animate(1.0);  
-
-
-
+function returnHtmlElementId(name) {
+  return document.getElementById(name);
+}
 
 // Parallax
 $( "#data-area" ).parallax({imageSrc: 'img/cidadeparallax.png'})
